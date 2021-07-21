5603c690b0a0bf18932b026c4016060a80ef5d7c
Lex Zar <gadkonb@ya.ru>
2021-07-21 17:58:01 +0300

    задача №7 рабаота банкомата через класс CashMachine

---
+++ task7_atm_cashmachine/task7.rb
@@ -0,0 +1,24 @@
+# Переписать банкомат на работу с классами
+# Класс должен называтся- CashMachine.
+# Программа должна запускатся с помощью метода класса init, 
+# создавать экземпляр класса и взаимодействовать с пользователем
+# соглассно условиям задачи
+
+require "./CashMachine"
+
+loop do
+    bancomat = CashMachine.new
+    input = gets.downcase.chomp
+
+    if input === "d"
+        bancomat.deposit
+    elsif  input === "w"
+        bancomat.withdraw   
+    elsif input ==="b" 
+        bancomat.balance
+    elsif input === "q" 
+        break
+    else
+        puts "Только D W B Q"  
+    end   
+end
